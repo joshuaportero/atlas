24680d0d8314b0543f46dfa570bd21afefff61c8
Joshua Portero <77929472+joshuaportero@users.noreply.github.com>
2025-01-08 13:22:56 -0500

    chore: align message formating

--- src/main/java/dev/portero/atlas/cmd/AtlasCMD.java
+++ src/main/java/dev/portero/atlas/cmd/AtlasCMD.java
@@ -3,15 +3,17 @@ package dev.portero.atlas.cmd;
 import dev.rollczi.litecommands.annotations.command.Command;
 import dev.rollczi.litecommands.annotations.context.Context;
 import dev.rollczi.litecommands.annotations.execute.Execute;
+import dev.rollczi.litecommands.annotations.permission.Permission;
 import io.papermc.paper.plugin.configuration.PluginMeta;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
 import static dev.portero.atlas.util.MessageUtil.centerDecorated;
 import static dev.portero.atlas.util.MessageUtil.format;
-import static net.kyori.adventure.text.format.NamedTextColor.AQUA;
+import static net.kyori.adventure.text.format.NamedTextColor.GRAY;
 
 @Command(name = "atlas")
+@Permission("atlas.cmd.base")
 public class AtlasCMD {
 
     private final Plugin plugin;
@@ -30,10 +32,10 @@ public class AtlasCMD {
         String version = pluginMeta.getVersion();
         String author = String.join(", ", pluginMeta.getAuthors());
 
-        sender.sendMessage(centerDecorated(AQUA, "&r&7[ &bAtlas33 &7→ &cINFO &7]"));
-        sender.sendMessage(format("&bName: &7" + name));
-        sender.sendMessage(format("&bVersion: &7" + version));
-        sender.sendMessage(format("&bAuthor: &7" + author));
-        sender.sendMessage(centerDecorated(AQUA, ""));
+        sender.sendMessage(centerDecorated(GRAY, "&r[ &aATLAS &7→ &cINFO &7]"));
+        sender.sendMessage(format("  • &aName: &7" + name));
+        sender.sendMessage(format("  • &aVersion: &7" + version));
+        sender.sendMessage(format("  • &aAuthor: &7" + author));
+        sender.sendMessage(centerDecorated(GRAY, ""));
     }
 }
